30cf459a41ba173a5e0e14e6b80e4859bc144c01
Ubuntu <ubuntu@ip-172-31-16-49.ec2.internal>
2020-12-01 16:31:44 +0000

    Add detail book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_164844) do
+ActiveRecord::Schema.define(version: 2020_12_01_163017) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_164844) do
     t.integer "number_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
